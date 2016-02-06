14447d94bd5ea25a4c1ee6ab00aaf0155914a2c0
tomoya <ihatov08@gmail.com>
2016-02-07 01:39:04 +0900

    第２課題ー問B実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206151919) do
+ActiveRecord::Schema.define(version: 20160206153846) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "tweet_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160206151919) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "image"
+    t.string   "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
